9f4bd20ade38151ee9d6b0ff8d2a49c0ed280d8f
Aman Upadhyay <146805438+amanupadhyay-obj@users.noreply.github.com>
2024-08-13 13:17:25 +0530

    making flicksfeed app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Head from './components/Head';
+import './App.css';
+import { Body } from './components/Body';
+
+function App() {
+  return (
+    <div >
+    <Head/>
+    <Body/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainContainer from './MainContainer'
+import Sidebar from './Sidebar';
+
+export const Body = () => {
+  return (
+    <div>
+        <Sidebar/>
+        <MainContainer/>
+    </div>
+  )
+}
