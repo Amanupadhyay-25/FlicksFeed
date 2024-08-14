d307ca2a5d4eddcc16abd73250bd35758523acad
Aman Upadhyay <146805438+amanupadhyay-obj@users.noreply.github.com>
2024-08-14 15:32:56 +0530

    onFocus+onBlur

--- src/components/Head.js
+++ src/components/Head.js
@@ -42,55 +42,58 @@ const toggleMenuHandler=()=>{
   dispatch(toggleMenu());
 }
 
-  return (
-    <div className='grid grid-flow-col p-5 m-2 shadow-lg' >
-      <div className='flex col-span-1 '>
-        <img 
-        onClick={()=>toggleMenuHandler()}
-       className=' h-8 cursor-pointer'
-        alt='hamburger menu'
+return (
+  <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+    <div className="flex col-span-1">
+      <img
+        onClick={() => toggleMenuHandler()}
+        className="h-8 cursor-pointer"
+        alt="menu"
         src={MENU_CDN}
-        />
+      />
+      <a href="/">
         <img
-       className='h-8 mx-2 cursor-pointer '
-        alt='youtube-logo'
-        src={FLICK_FEED}
+          className="h-8 mx-2"
+          alt="youtube-logo"
+          src={FLICK_FEED}
         />
-        </div>
-        <div className='col-span-10 text-center'>
-        <div>
+      </a>
+    </div>
+    <div className="col-span-10 px-10">
+      <div>
         <input
-        value={searchQuery}
-        className='px-5 w-1/2 border border-gray-400 p-2 rounded-l-full'
-        type='text'
-        onChange={(e)=>setSearchQuery(e.target.value)}
-        onFocus={()=>setShowSuggestions(true)}
-        onBlur={()=>setShowSuggestions(false)}
+          className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onFocus={() => setShowSuggestions(true)}
+          onBlur={() => setShowSuggestions(false)}
         />
-        <button className='border border-gray-400 px-5 py-2 rounded-r-3xl bg-gray-200'>🔍</button>
-        </div>
-        {showSuggestions && (
-          <div className="fixed bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
-            <ul>
-              {suggestions.map((s) => (
-                <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
-                  🔍 {s}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-
-        </div>
-        <div className='col-span-1'>
-          <img
-           className=' h-8'
-          alt='user-logo'
-          src={USER_LOGO}
-          />
-        </div>
+        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+          🔍
+        </button>
       </div>
-  )
-}
+      {showSuggestions && (
+        <div className="fixed bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
+          <ul>
+            {suggestions.map((s) => (
+              <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+                🔍 {s}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+    <div className="col-span-1">
+      <img
+        className="h-8"
+        alt="user"
+        src={USER_LOGO}
+      />
+    </div>
+  </div>
+);
+};
 
-export default Head
+export default Head;
